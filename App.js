81ceca6c1a5beb80efa16a4096151ab06b2b9ed8
sungj <cjssus23@gmail.com>
2018-12-24 14:20:25 +0900

    middle save

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
 import React , {Component} from 'react';
-import {StyleSheet, Text, View, Image, ActivityInicator} from 'react-native';
+import {StyleSheet, Text, View, StatusBar} from 'react-native';
 import Weather from './commponents/Weather'
 
 class App extends Component {
   state = {
-    isLoaded:false
+    isLoaded:false,
+    latitude:null,
+    longtitude:null
   };
 
+  componentDidMount = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) =>{
+        console.log(position);
+        this.setState({
+          latitude:position.coords.latitude,
+          longtitude:position.coords.longitude,
+          isLoaded:true
+        });
+        error => {
+          console.log(error);
+        }
+      }
+    )
+  }
   render(){
     const {isLoaded}= this.state;
     return (
       <View style={styles.container}>
-        {isLoaded ? null : ( 
-      <Weather/>
-      )}   
+      <StatusBar hidden={false} />
+        {isLoaded ? (<Weather/>) : ( 
+          <View>
+            <Text style={styles.loadingText}>날씨 정보를 불러오는 중입니다....</Text>
+          </View>
+      )}
+
       </View>
     )
   }
--- commponents/Weather.js
+++ commponents/Weather.js
@@ -1,25 +1,98 @@
 import React, { Component } from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {LinearGradient} from "expo";
+import {MaterialCommunityIcons} from '@expo/vector-icons';
+import PropTypes from 'prop-types';
 
+const WeatherCases={
+    Rain:{
+        colors:['#00c6Fb', '#005BEA'],
+        title:"It's raning man! O.M.G!!! is raining man!",
+        subtitle:"하늘에서 씨꺼먼 남정네가 내려와 -_-;;;;;",
+        icon:"weather-rainy"
+    },
+
+    Clear:{
+        colors:['#FEF253', '#FF7300'],
+        title:"햇볕은 쨍쨍 모래알은 반짝!",
+        subtitle:"당신의 피부에 자외선 빔~!!",
+        icon:"weather-sunny"
+    },
+
+    ThunderStorms:{
+        colors:['#00ECBC', '#007ADF'],
+        title:"Thunder, feel the thunder, Lightning and the thunder ",
+        subtitle:"Imagine Dragons!! I like You!!!! holly-molly!!",
+        icon:"weather-lightning"
+    },
+
+    Clouds: {
+        colors: ["#D7D2CC", "#304352"],
+        title: "Clouds",
+        subtitle: "I know, fucking boring",
+        icon: "weather-cloudy"
+    },
+    Snow: {
+        colors: ["#7DE2FC", "#B9B6E5"],
+        title: "Cold as balls",
+        subtitle: "Do you want to build a snowman? Fuck no.",
+        icon: "weather-snowy"
+    },
+    Drizzle: {
+        colors: ["#89F7FE", "#66A6FF"],
+        title: "Drizzle",
+        subtitle: "Is like rain, but gay 🏳️‍🌈",
+        icon: "weather-hail"
+    },
+    Haze: {
+        colors: ["#89F7FE", "#66A6FF"],
+        title: "Haze",
+        subtitle: "Don't know what that is 💩",
+        icon: "weather-hail"
+    },
+    Mist: {
+        colors: ["#D7D2CC", "#304352"],
+        title: "Mist!",
+        subtitle: "It's like you have no glasses on.",
+        icon: "weather-fog"
+    }
+};
+
+function Weather({WeatherName, temp}){
+    return (
+        <LinearGradient colors={WeatherCases[WeatherName].colors} style={styles.container}>
+        <View style={styles.upper}>
+            <MaterialCommunityIcons color={white} size={144} name={WeatherCases[WeatherName].icon}/>
+            <Text style ={styles.temperature}> {temp} °C </Text>
+        </View>
+        <View style ={styles.lower}>
+            <Text style={styles.title}>{WeatherCases[WeatherName].title}</Text>
+            <Text style={styles.subtitle}>{WeatherCases[WeatherName].subtitle}</Text>
+        </View>
+        </LinearGradient>
+    )
+};
+
+/*
 class Weather extends Component {
     render(){
         return (
-            <LinearGradient colors={["green", "white"]} style={styles.container}>
+            <LinearGradient colors={["red", "green", "white", "green", "red"]} style={styles.container}>
             <View style={styles.upper}>
-                <Text>날씨 아이콘</Text>
+                <Ionicons name="ios-snow" size={130} color='white'/>
+                <Text style ={styles.temperature}> 100°C</Text>
             </View>
 
             <View style={styles.lower}>
-                <Text style={styles.title}>날씨 알리미</Text>
-                <Text style={styles.subtitle}>날씨가 궁금해? 그럼 창밖을 봐!!</Text>
+                <Text style={styles.title}>Awesome White X-mas!!</Text>
+                <Text style={styles.subtitle}>solo chun-guk! couple zi-ok!!!</Text>
             </View>
 
             </LinearGradient>
         );
     }
 }
-
+*/
 const styles = StyleSheet.create({
     container:{
         flex:1,
@@ -28,7 +101,14 @@ const styles = StyleSheet.create({
     upper :{
         flex:1,
         justifyContent:"center",
-        alignItems:"center"
+        alignItems:"center",
+        backgroundColor: "transparent"
+    },
+
+    temperature :{
+        fontSize : 48,
+        color: 'yellow',
+
     },
 
     lower :{
@@ -49,7 +129,7 @@ const styles = StyleSheet.create({
     subtitle:{
         fontSize:24,
         backgroundColor:"transparent",
-        color:"red",
+        color:"pink",
         marginBottom: 100
     }
 })
